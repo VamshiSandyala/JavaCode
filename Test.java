37301b493e250ca14611de7860f3fa4b4f38ca9b
Vamshi S <117306255+vamshis26@users.noreply.github.com>
2023-09-29 15:37:09 -0500

    Updated Test.java with some methods

--- Test.java
+++ Test.java
@@ -1,5 +1,13 @@
 public class Test {
+    public static void add(){
+        int a=10,b=20;
+        int sum=a+b;
+        System.out.println("Sum of two: "+sum);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        //creating instance
+        Test t=new Test();
+        t.add();
     }
 }
