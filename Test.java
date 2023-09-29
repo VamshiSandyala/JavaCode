bfb33591ef2d5a37e4d2376c45beb299563e9ea5
VamshiSandyala <114252962+VamshiSandyala@users.noreply.github.com>
2023-09-29 15:49:58 -0500

    Updated Test.java and sent to owner of the original file with adding div method.

--- Test.java
+++ Test.java
@@ -9,10 +9,16 @@ public class Test {
         int diff=a-b;
         System.out.println("Diff of two: "+diff);
     }
+     public static void div(){
+        int a=10,b=20;
+        int div=a/b;
+        System.out.println("div of two: "+div);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Test t=new Test();
         t.sub();
         t.add();
+        t.div();
     }
 }
