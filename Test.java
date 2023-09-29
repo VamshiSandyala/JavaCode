f152a3b16690af1b53ea59a00d54e0fa68f0b612
Vamshi S <117306255+vamshis26@users.noreply.github.com>
2023-09-29 15:44:18 -0500

    Updated Test.java with few changes

--- Test.java
+++ Test.java
@@ -7,12 +7,12 @@ public class Test {
     public static void sub(){
         int a=10,b=20;
         int diff=a-b;
-        System.out.println("Sum of two: "+diff);
+        System.out.println("Diff of two: "+diff);
     }
     public static void main(String[] args) {
         System.out.println("Hello world!");
         Test t=new Test();
-        t.diff();
+        t.sub();
         t.add();
     }
 }
