87e2c8de338036046e76aad7da049878d2bbb63f
Vamshi S <117306255+vamshis26@users.noreply.github.com>
2023-09-29 15:40:05 -0500

    Updated Test.java add included difference method.

--- Test.java
+++ Test.java
@@ -1,5 +1,12 @@
 public class Test {
+    public static void sub(){
+        int a=10,b=20;
+        int diff=a-b;
+        System.out.println("Sum of two: "+diff);
+    }
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        Test t=new Test();
+        t.diff();
     }
 }
